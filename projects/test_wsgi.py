1ed55ed31274ba802408720d2fde40174e3fda1d
Sie <nsi@nbim.no>
2021-08-15 22:46:00 +0800

    Try with version output

--- projects/test_wsgi.py
+++ projects/test_wsgi.py
@@ -1,6 +1,7 @@
+import sys
 def application(environ, start_response):
     status = '200 OK'
-    output = b'Hooray, mod_wsgi is working'
+    output = b'Hooray, mod_wsgi is working with ' + sys.version
 
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
