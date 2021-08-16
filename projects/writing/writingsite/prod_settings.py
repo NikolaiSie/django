4bd9ff6695b686adef68bf61617d8f66c2a48b30
Sie <nsi@nbim.no>
2021-08-16 11:18:26 +0800

    Basic simple key handling

--- projects/writing/writingsite/prod_settings.py
+++ projects/writing/writingsite/prod_settings.py
@@ -9,14 +9,14 @@ from .base_settings import *
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = '1c305b704417038697acaa7eda5e2e3db063157d2d9e9344f87fef76bd7ec1e2c58348f628caa12e92ec28f1b7b1987a99077feee4cd66fd12c939ef68031384'
+#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
 ALLOWED_HOSTS = ['122.248.208.99', 'sigurdvidarsson.com', 'www.sigurdvidarsson.com', '127.0.0.1']
-
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
