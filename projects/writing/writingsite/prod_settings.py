6cee602a14d9bbaffaf562755800402e12347247
Sie <nsi@nbim.no>
2021-08-16 14:36:06 +0800

    Go back

--- projects/writing/writingsite/prod_settings.py
+++ projects/writing/writingsite/prod_settings.py
@@ -1,11 +1,10 @@
 
 
-import os
 from .base_settings import *
 
 
 DEBUG = False
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
