5c87a00614df10126f1fbfc2dfbd9464e3e61926
Nikolai Sie <nikolai.sie@gmail.com>
2022-10-04 04:06:51 +0800

    Restore

--- projects/writing/writingsite/base_settings.py
+++ projects/writing/writingsite/base_settings.py
@@ -12,6 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+SECRET_KEY = "1c305b704417038697acaa7eda5e2e3db063157d2d9e9344f87fef76bd7ec1e2c58348f628caa12e92ec28f1b7b1987a99077feee4cd66fd12c939ef68031384"
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['122.248.208.99', 'sigurdvidarsson.com', 'www.sigurdvidarsson.com', '127.0.0.1']
--- projects/writing/writingsite/prod_settings.py
+++ projects/writing/writingsite/prod_settings.py
@@ -1,11 +1,9 @@
 
 
 from .base_settings import *
-import boto3
+#import boto3
 
 #ssm = boto3.client("ssm", region_name='ap-southeast-1')
 #SECRET_KEY = ssm.get_parameter(Name="/django-lightsail-key", WithDecryption=True)["Parameter"]["Value"]
-
-SECRET_KEY = "1c305b704417038697acaa7eda5e2e3db063157d2d9e9344f87fef76bd7ec1e2c58348f628caa12e92ec28f1b7b1987a99077feee4cd66fd12c939ef68031384"
 
 DEBUG = False
