e215a095b8754ff66ec1b32ee8413ff1c8813f42
Nikolai Sie <nikolai.sie@gmail.com>
2022-10-03 18:52:06 +0800

    Remove static dirs

--- projects/writing/writingsite/base_settings.py
+++ projects/writing/writingsite/base_settings.py
@@ -111,7 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR.as_posix() + '/static'
-STATICFILES_DIRS = [BASE_DIR.as_posix() + '/sigurd/static']
 STATIC_URL = '/static/'
 
 # Default primary key field type
