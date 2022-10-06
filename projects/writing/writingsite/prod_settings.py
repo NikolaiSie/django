2d9d0308f788bb89192bf379147ca2dd8dab9533
Nikolai Sie <nikolai.sie@gmail.com>
2022-10-06 18:38:00 +0800

    Retry ssm

--- projects/writing/writingsite/prod_settings.py
+++ projects/writing/writingsite/prod_settings.py
@@ -1,9 +1,9 @@
 
 
 from .base_settings import *
-#import boto3
+import boto3
 
-#ssm = boto3.client("ssm", region_name='ap-southeast-1')
-#SECRET_KEY = ssm.get_parameter(Name="/django-lightsail-key", WithDecryption=True)["Parameter"]["Value"]
+ssm = boto3.client("ssm", region_name='ap-southeast-1')
+SECRET_KEY = ssm.get_parameter(Name="/django-lightsail-key", WithDecryption=True)["Parameter"]["Value"]
 
 DEBUG = False
