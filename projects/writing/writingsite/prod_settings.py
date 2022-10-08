964d8fce16b34fa33165a11814ed4e77ef141f91
Nikolai Sie <nikolai.sie@gmail.com>
2022-10-08 10:00:28 +0800

    Restore again

--- projects/writing/writingsite/prod_settings.py
+++ projects/writing/writingsite/prod_settings.py
@@ -1,7 +1,6 @@
 
 
 from .base_settings import *
-import boto3
 
 #ssm = boto3.client("ssm", region_name='ap-southeast-1')
 #SECRET_KEY = ssm.get_parameter(Name="/django-lightsail-key", WithDecryption=True)["Parameter"]["Value"]
